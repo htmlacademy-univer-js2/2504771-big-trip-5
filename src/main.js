6e23b7ec246fb7964706069e409af3daf0d162e0
Андрей <5procentlife@gmail.com>
2025-05-25 22:58:26 +0500

    л

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('hello, world!')
+import Presenter from './presenter/main-presenter';
+import PointsModel from './model/points-model';
+
+const tripControlFilters = document.querySelector('.trip-controls__filters');
+const tripEvents = document.querySelector('.trip-events');
+
+const pointsModel = new PointsModel();
+
+const presenter = new Presenter(tripControlFilters, tripEvents, pointsModel);
+
+presenter.init();
---
+++ src/view/edit-form-view.js
@@ -0,0 +1,113 @@
+import { createElement } from '../render';
+import { getFullDate } from '../utils.js';
+import { EVENT_TYPES, CITIES } from '../consts.js';
+
+function createEditFormTamplate(point) {
+  const { type, destination, dateFrom, dateTo, basePrice, offers } = point;
+
+  const fullStartDate = getFullDate(dateFrom);
+  const fullEndDate = getFullDate(dateTo);
+
+
+  return (
+    `<li class="trip-events__item">
+    <form class="event event--edit" action="#" method="post">
+                <header class="event__header">
+                  <div class="event__type-wrapper">
+                    <label class="event__type  event__type-btn" for="event-type-toggle-1">
+                      <span class="visually-hidden">Choose event type</span>
+                      <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+                    </label>
+                    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+
+                    <div class="event__type-list">
+                      <fieldset class="event__type-group">
+                        <legend class="visually-hidden">Event type</legend>
+                        ${EVENT_TYPES.map((eventType) => `<div class="event__type-item">
+                          <input id="event-type-${eventType.toLowerCase()}-1" class="event__${eventType.toLowerCase()}-input  visually-hidden" type="radio" name="event-type" value="${eventType.toLowerCase()}">
+                          <label class="event__type-label  event__type-label--${eventType.toLowerCase()}" for="event-type-${eventType.toLowerCase()}-1">${eventType}</label>
+                          </div>`).join('')};)}
+                      </fieldset>
+                    </div>
+                  </div>
+
+                  <div class="event__field-group  event__field-group--destination">
+                    <label class="event__label  event__type-output" for="event-destination-1">
+                        ${type}
+                    </label>
+                    <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+                    <datalist id="destination-list-1">
+                        ${CITIES.map((city) => `<option value="${city}"></option>`).join('')}
+                    </datalist>
+                  </div>
+
+                  <div class="event__field-group  event__field-group--time">
+                    <label class="visually-hidden" for="event-start-time-1">From</label>
+                    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${fullStartDate}">
+                    &mdash;
+                    <label class="visually-hidden" for="event-end-time-1">To</label>
+                    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${fullEndDate}">
+                  </div>
+
+                  <div class="event__field-group  event__field-group--price">
+                    <label class="event__label" for="event-price-1">
+                      <span class="visually-hidden">Price</span>
+                      &euro;
+                    </label>
+                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
+                  </div>
+
+                  <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+                  <button class="event__reset-btn" type="reset">Delete</button>
+                  <button class="event__rollup-btn" type="button">
+                    <span class="visually-hidden">Open event</span>
+                  </button>
+                </header>
+                <section class="event__details">
+                  <section class="event__section  event__section--offers">
+                    <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+
+                    <div class="event__available-offers">
+                      ${offers.map((offer) => `<div class="event__offer-selector">
+                        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.title.toLowerCase()}-1" type="checkbox" name="event-offer-${offer.title.toLowerCase()}" checked>
+                        <label class="event__offer-label" for="event-offer-${offer.title.toLowerCase()}-1">
+                          <span class="event__offer-title">${offer.title}</span>
+                          &plus;&euro;&nbsp;
+                          <span class="event__offer-price">${offer.price}</span>
+                        </label>
+                      </div>`).join('')}
+                    </div>
+                  </section>
+
+                  <section class="event__section  event__section--destination">
+                    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+                    <p class="event__destination-description">${destination.description}</p>
+                  </section>
+                </section>
+              </form>
+            </li>`
+  );
+}
+
+export default class EditForm {
+
+  constructor({ point }) {
+    this.point = point;
+  }
+
+  getTemplate() {
+    return createEditFormTamplate(this.point);
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
