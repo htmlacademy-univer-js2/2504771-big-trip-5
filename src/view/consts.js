a244b09c163b9c4effa7a7b1b36f657fa13122a4
Андрей <5procentlife@gmail.com>
2025-06-09 13:07:06 +0500

    Пришёл, увидел, загрузил (часть 2)

--- src/view/consts.js
+++ src/view/consts.js
@@ -1,28 +1,17 @@
-const CITIES = ['Berlin', 'Stuttgart', 'Minsk', 'Köln', 'London', 'Lissabon', 'Irkutsk', 'Sydney'];
-
-const EVENT_TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
-
-const DESCRIPTIONS = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  'Cras aliquet varius magna, non porta ligula feugiat eget.',
-  'Fusce tristique felis at fermentum pharetra.',
-  'Aliquam id orci ut lectus varius viverra.',
-  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
-  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
-  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
-  'Sed sed nisi sed augue convallis suscipit in sed felis.',
-  'Aliquam erat volutpat.',
-  'Nunc fermentum tortor ac porta dapibus.',
-  'In rutrum ac purus sit amet tempus.',
-];
-
-const OFFERS = ['Rent a car', 'Add luggage', 'Add breakfast', 'Switch to business', 'Upgrade to business class'];
+const EVENT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 
 const FILTER_TYPES = {
-  EVERYTHING: 'everything',
-  FUTURE: 'future',
-  PRESENT: 'present',
-  PAST: 'past',
+  EVERYTHING: 'EVERYTHING',
+  FUTURE: 'FUTURE',
+  PRESENT: 'PRESENT',
+  PAST: 'PAST'
+};
+
+const EMPTY_LIST_MESSAGES = {
+  [FILTER_TYPES.EVERYTHING]: 'Click New Event to create your first point',
+  [FILTER_TYPES.PAST]: 'There are no past events now',
+  [FILTER_TYPES.PRESENT]: 'There are no present events now',
+  [FILTER_TYPES.FUTURE]: 'There are no future events now'
 };
 
 const SORT_TYPES = {
@@ -36,4 +25,62 @@ const MODE = {
   EDITING: 'EDITING',
 };
 
-export { CITIES, EVENT_TYPES, DESCRIPTIONS, OFFERS, FILTER_TYPES, SORT_TYPES, MODE };
+const ACTIONS = {
+  UPDATE_POINT: 'UPDATE_POINT',
+  DELETE_POINT: 'DELETE_POINT',
+  ADD_POINT: 'ADD_POINT',
+};
+
+const UPDATE_TYPES = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+  INIT: 'INIT',
+};
+
+const EMPTY_POINT = {
+  type: 'flight',
+  destination: null,
+  dateFrom: null,
+  dateTo: null,
+  basePrice: 0,
+  offers: [],
+  isFavorite: false,
+};
+
+const FORM_TYPE = {
+  CREATE: 'CREATE',
+  EDIT: 'EDIT',
+};
+
+const METHOD = {
+  GET: 'GET',
+  PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
+};
+
+const TIME_LIMIT = {
+  LOWER_LIMIT: 200,
+  UPPER_LIMIT: 500,
+};
+
+const AUTHORIZATION = 'Basic fra114a3i8a7t0rr';
+
+const END_POINT = 'https://21.objects.htmlacademy.pro/big-trip';
+
+export {
+  EVENT_TYPES,
+  FILTER_TYPES,
+  EMPTY_LIST_MESSAGES,
+  SORT_TYPES,
+  MODE,
+  ACTIONS,
+  UPDATE_TYPES,
+  EMPTY_POINT,
+  FORM_TYPE,
+  METHOD,
+  AUTHORIZATION,
+  END_POINT,
+  TIME_LIMIT
+};
